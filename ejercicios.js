b3ac36c945526a7cb044566ab77c092395e977e0
EdwuardTSW <luis.chay@tecdesoftware.edu.mx>
2025-12-03 09:48:33 -0600

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -1,6 +1,6 @@
 // FUNDAMENTOS DE PROGRAMACIÓN - PRÁCTICA
 // Nombre del estudiante: _________________
-// Fecha: _________________
+// Fecha: ________________
 
 // ============================================
 // SECCIÓN 1: VARIABLES Y TIPOS DE DATOS (10 puntos)
